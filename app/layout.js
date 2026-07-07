258f7e838c022b47dd027e89d8f1be9e38c4acc3
Charbel Merza <charbelmerza3@gmail.com>
2026-07-07 18:16:33 +0300

    Fix mobile layout margin

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export default function RootLayout({ children }) {
           <div className="flex min-h-screen">
             <Sidebar />
 
-            <main className="flex-1 ml-64 overflow-x-hidden">
+            <main className="flex-1 overflow-x-hidden lg:ml-64">
   {children}
 </main>
           </div>
